b2d22564e5e4ef92544ab4d39faa7aea3dd9fffc
Facundo Gervasi <facugervasi9@gmail.com>
2023-09-28 13:52:13 -0300

    update 0.1
    Reorganized files
    Added Views (todo buttons on small sidebar)
    Remade Index.js as a parent module

---
+++ src/ProductViews/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeView = () => {
+  return(
+    <div>
+         <h1>jome</h1>
+         <p>hgolasdjaslkjdlasjdasjdasldasdadasda</p>
+    </div>
+  );
+};
+
+export default HomeView;
---
+++ src/ProductViews/iphone.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const iphone = () => {
+  return(
+    <div>
+         <h1>ñaisssfon</h1>
+         <p>hgolasdjaslkjdlasjdasjdasldasdadasda</p>
+    </div>
+  );
+};
+
+export default iphone;
