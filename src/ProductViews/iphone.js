3f9d75b260d44a792375fd6c175296cbd10c903c
Luciano <91286415+BugaToro@users.noreply.github.com>
2023-10-05 01:41:41 -0300

    Update 0.3
    a) Autogeneración de cards. 
    b) Rediseño de cards.
    c) Generación de overlay.
    d) Identificación por keys de divs.

--- src/ProductViews/iphone.js
+++ src/ProductViews/iphone.js
@@ -1,45 +1,55 @@
-import React from "react";
-import "./../styles/Products.css";
-import Imagen from "./../images/prueba1.jpg"
+import React, { useState } from 'react';
+import './../styles/Products.css';
 
 const Iphone = () => {
+  const [selectedDiv, setSelectedDiv] = useState(null); // Estado para almacenar el div seleccionado
+
+  const divArray = Array.from({ length: 10 }, (_, index) => index + 1);
+
+  const selection = (event) => {
+    const selDiv = event.target;
+    setSelectedDiv(selDiv);
+    hidecard();
+  };
+  const closeOverlay = () => {
+    setSelectedDiv(null);
+    showcard();
+  };
+  const generateContent = () => {
+    if (selectedDiv) {
+      // Aquí puedes generar contenido basado en el div seleccionado
+      return (
+        <div className="overlay" onClick={closeOverlay}>
+          <h1>{selectedDiv.textContent}</h1>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <div className="category-container">
-      <div className="card-container"style={{ backgroundImage: `url('${Imagen}')` }}>
-      <span >Iphone 1</span>
-      </div>
-      <div className="card-container">
-        <span>Iphone 3</span>
-      </div>
-      <div className="card-container">
-        <span>Iphone 4</span>
-      </div>
-      <div className="card-container">
-        <span>Iphone 5</span>
-      </div>
-      <div className="card-container">
-        <span>Iphone 6</span>
-      </div>
-      <div className="card-container">
-        <span>Iphone 7</span>
-      </div>
-      <div className="card-container">
-        <span>Iphone 8</span>
-      </div>
-      <div className="card-container">
-        <span>Iphone 9</span>
-      </div>
-      <div className="card-container">
-        <span>Iphone 10</span>
-      </div>
-      <div className="card-container">
-        <span>Iphone 11</span>
-      </div>
-      <div className="card-container">
-        <span>Iphone XR</span>
-      </div>
+      {divArray.map((divNumber) => (
+        <div className="card-container show-card" key={divNumber} id="card-hover" onClick={selection}>
+          <span>Iphone {divNumber}</span>
+        </div>
+      ))}
+      {generateContent()}
     </div>
   );
+};
+function showcard() {
+  var cards = document.getElementsByClassName("card-container");
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].classList.add("show-card");
+  };
+};
+
+function hidecard() {
+  var cards = document.getElementsByClassName("card-container");
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].classList.remove("show-card");
+  };
 };
 
 export default Iphone;
