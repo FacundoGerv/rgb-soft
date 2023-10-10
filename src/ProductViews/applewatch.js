8f12b3ccb3aaf1a54684931d4661d90554f937f3
Facundo Gervasi <facugervasi9@gmail.com>
2023-10-10 13:50:29 -0300

    Update 4.0
    Added homepage.
    Fixed some code.
    Added cards on every product view

--- src/ProductViews/applewatch.js
+++ src/ProductViews/applewatch.js
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useState } from 'react';
+import './../styles/Products.css';
 
-const applewatch = () => {
-  return(
-    <div>
-         <h1>applewatch</h1>
-         <p>hgolasdjaslkjdlasjdasjdasldasdadasda</p>
+const Applewatch = () => {
+  const [selectedDiv, setSelectedDiv] = useState(null); 
+
+  const divArray = Array.from({ length: 12 }, (_, index) => index + 1);
+
+  const selection = (event) => {
+    const selDiv = event.target;
+    setSelectedDiv(selDiv);
+    hidecard();
+  };
+  const closeOverlay = () => {
+    setSelectedDiv(null);
+    showcard();
+  };
+  const generateContent = () => {
+    if (selectedDiv) {
+
+      return (
+        <div className="overlay" onClick={closeOverlay}>
+          <h1>{selectedDiv.textContent}</h1>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div className="product-container">
+    <div className="category-container">
+    <h1 className="category-title">Apple Watch</h1>
+      {divArray.map((divNumber, index) => (
+        <div
+          className={"card-container show-card"}
+          key={divNumber}
+          id="card-hover"
+          onClick={selection}
+        >
+          <span>placeholder {divNumber}</span>
+        </div>
+      ))}
+      
+    </div>
+    {generateContent()}
     </div>
   );
 };
+function showcard() {
+  var cards = document.getElementsByClassName("card-container");
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].classList.add("show-card");
+  };
+};
+function hidecard() {
+  var cards = document.getElementsByClassName("card-container");
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].classList.remove("show-card");
+  };
+};
 
-export default applewatch;
+export default Applewatch;
