ce92b3685a97bfbb5e0fca8a6a347be6cd0afdc8
Luciano <91286415+BugaToro@users.noreply.github.com>
2023-10-24 22:54:51 -0300

    Update-4.1
    Cambios realizados:
    
    1) Se creó un estado selectedProduct para mantener el producto seleccionado.
    2) Se modificó la función selection para que acepte un producto como argumento y lo establezca como el producto seleccionado.
    3) Se modificó la función closeOverlay para que limpie el producto seleccionado.
    4) Se actualizó generateContent para que utilice selectedProduct en lugar de product y mostrar la información correspondiente.

--- src/ProductViews/iphone.js
+++ src/ProductViews/iphone.js
@@ -1,25 +1,105 @@
 import React, { useState } from 'react';
 import './../styles/Products.css';
+import whatsappIcon from "./../images/whatsapp.png";
+import Backicon from "./../images/back-icon.png"
 
 const Iphone = () => {
-  const [selectedDiv, setSelectedDiv] = useState(null); 
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
-  const divArray = Array.from({ length: 10 }, (_, index) => index + 1);
+  const [product, setProduct] = useState({
+    "product": [
+      {
+        "category": "iphone",
+        "title": "Iphone SE",
+        "memory_ram": "2gb Ram",
+        "color": "azul turquesa",
+        "memory": "64gb",
+        "price": 67999,
+        "camera": "18px",
+        "description": "El iPhone dispone de cámara de fotos y un reproductor de música (equivalente al del iPod), además de software para enviar y recibir mensajes de texto y de voz. También ofrece servicios de Internet, como enviar, recibir y leer correo electrónico, cargar páginas web y conectividad por Wi-Fi.",
+        "vendido": true,
+        "image": 'prueba1.jpg',
+      },
+      {
+        "category": "iphone",
+        "title": "Iphone 12 PRO MAX",
+        "memory_ram": "2gb Ram",
+        "color": "azul turquesa",
+        "memory": "64gb",
+        "price": 67999,
+        "camera": "18px",
+        "description": "descripción 2",
+        "vendido": false,
+        "image": 'prueba2.jpg',
+      },
+      {
+        "category": "iphone",
+        "title": "Iphone 11",
+        "memory_ram": "2gb Ram",
+        "color": "azul turquesa",
+        "memory": "64gb",
+        "price": 67999,
+        "camera": "18px",
+        "description": "descripción 3",
+        "vendido": false,
+        "image": 'prueba3.jpg',
+      },
+      {
+        "category": "airpods",
+        "title": "Iphone 2",
+        "memory_ram": "2gb Ram",
+        "color": "azul turquesa",
+        "memory": "64gb",
+        "price": 67999,
+        "camera": "18px",
+        "description": "descripción 2",
+        "vendido": false,
+      }
+    ]
+  });
 
-  const selection = (event) => {
-    const selDiv = event.target;
-    setSelectedDiv(selDiv);
+  const iphoneProducts = product.product.filter(item => item.category === "iphone");
+
+  const selection = (selectedProduct) => {
+    setSelectedProduct(selectedProduct);
     hidecard();
   };
+
   const closeOverlay = () => {
-    setSelectedDiv(null);
-    showcard();
+    if (selectedProduct != null) {
+      setSelectedProduct(null);
+      showcard();
+    }
   };
+
   const generateContent = () => {
-    if (selectedDiv) {
+    if (selectedProduct) {
       return (
-        <div className="overlay" onClick={closeOverlay}>
-          <h1>{selectedDiv.textContent}</h1>
+        <div className="overlay">
+          <div className='inner-title'>
+            <h1>{selectedProduct.title}</h1>
+          </div>
+          <div className='inner-overlay'>
+            <img className="product-image" src={require("./../images/" + selectedProduct.image)} alt="Descripción de la imagen" />
+            <div className="product-info-list">
+              <span>Memoria RAM: {selectedProduct.memory_ram}</span>
+              <span>Color: {selectedProduct.color}</span>
+              <span>Memoria: {selectedProduct.memory}</span>
+              <span>Precio: {selectedProduct.price}</span>
+              <span>Cámara: {selectedProduct.camera}</span>
+            </div>
+          </div>
+          <div className='inner-text'>
+            <span>{selectedProduct.description}</span>
+          </div>
+          <div className='button-container'>
+            <div className="whatsapp-button" onClick={sendWhatsAppMessage}>
+              <img src={whatsappIcon} />
+              <span>Whatsapp</span>
+            </div>
+          </div>
+          <div>
+          </div>
         </div>
       );
     }
@@ -27,36 +107,49 @@ const Iphone = () => {
   };
 
   return (
-    <div className="product-container">
-    <div className="category-container">
-      <h1 className="category-title">Iphone</h1>
-      {divArray.map((divNumber, index) => (
-        <div
-          className={"card-container show-card"}
-          key={divNumber}
-          id="card-hover"
-          onClick={selection}
-        >
-          <span>placeholder {divNumber}</span>
-        </div>
-      ))}
-      
-    </div>
-    {generateContent()}
+    <div onClick={closeOverlay} className="product-container">
+      <div className="category-container">
+        <h1 className="category-title">Iphone</h1>
+        {iphoneProducts.map((product, index) => (
+          <div
+            className={"card-container show-card"}
+            key={index}
+            id="card-hover"
+            onClick={() => selection(product)}
+          >
+            <span>{product.title}</span>
+            <div className="image-container">
+              <img className="product-image" src={require("./../images/" + product.image)} alt="Descripción de la imagen" />
+            </div>
+          </div>
+        ))}
+      </div>
+      {generateContent()}
     </div>
   );
 };
+
 function showcard() {
   var cards = document.getElementsByClassName("card-container");
   for (var i = 0; i < cards.length; i++) {
     cards[i].classList.add("show-card");
   };
 };
+
 function hidecard() {
   var cards = document.getElementsByClassName("card-container");
   for (var i = 0; i < cards.length; i++) {
     cards[i].classList.remove("show-card");
   };
+};
+
+const sendWhatsAppMessage = (selectedProduct) => {
+  const phoneNumber = '2604677605';
+  const message = {selectedProduct};
+
+  const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+
+  window.open(whatsappLink, '_blank');
 };
 
 export default Iphone;
