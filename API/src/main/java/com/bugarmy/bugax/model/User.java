f78586db12a7dde4c64b5047f3f3552148e7c31e
Luciano Bugarin <91286415+BugaToro@users.noreply.github.com>
2023-11-09 20:30:04 -0300

    API files

---
+++ API/src/main/java/com/bugarmy/bugax/model/User.java
@@ -0,0 +1,41 @@
+package com.bugarmy.bugax.model;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name = "users")
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private final long id;
+    @Column
+	private final String name;
+    @Column
+	private final String password;
+	
+	public User(long id, String name, String password) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+}
